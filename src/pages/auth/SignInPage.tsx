0d7adce0cfcbf2be4cd996d8b1ac85ed8187c734
mmvergara <mark.jesusmanabat@gmail.com>
2024-06-15 17:46:03 +0800

    Add form validation and sign-in functionality

--- src/pages/auth/SignInPage.tsx
+++ src/pages/auth/SignInPage.tsx
@@ -1,15 +1,50 @@
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { FirebaseAuth } from "../../firebase";
 
 const SignInPage = () => {
+  const [formValues, setFormValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormValues({ ...formValues, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      await signInWithEmailAndPassword(
+        FirebaseAuth,
+        formValues.email,
+        formValues.password
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <main>
       <Link className="home-link" to="/">
         ◄ Home
       </Link>
-      <form className="main-container">
+      <form className="main-container" onSubmit={handleSubmit}>
         <h1 className="header-text">Sign In</h1>
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Password" />
+        <input
+          name="email"
+          onChange={handleInputChange}
+          type="email"
+          placeholder="Email"
+        />
+        <input
+          name="password"
+          onChange={handleInputChange}
+          type="password"
+          placeholder="Password"
+        />
         <button type="submit">Login</button>
         <Link className="auth-link" to="/auth/sign-up">
           Don't have an account? Sign Up
